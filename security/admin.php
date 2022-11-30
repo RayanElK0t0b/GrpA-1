53d3c1d71e0627ca3c959c6f55d1a2e204382f7f
Perlainpin_ <greboul@gaming.tech>
2022-11-30 11:52:04 +0100

    mod admin.php

--- security/admin.php
+++ security/admin.php
@@ -1,13 +1,8 @@
 <?php 
 require_once "config.php";
 
-$sql = "SELECT * FROM user WHERE email='".$_POST['email']."' AND password='".$_POST['password']."'"; 
-$pre = $pdo->prepare($sql); 
-$pre->execute();
-$user = $pre->fetch(PDO::FETCH_ASSOC);
-
 if( isset($_SESSION['user'])){
-    if($user['admin'] == 1 ){ //vérifie si le user est un admin
+    if($_SESSION['user']['admin'] == 1 ){ //vérifie si le user est un admin
         header('Location:index.php');//on le redirige sur le panel admin
 
     }else{
