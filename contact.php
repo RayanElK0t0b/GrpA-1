1a3e1d14502b4de26640dfab48430e1ec6a31b63
Rayan El Kotob <relkotob@gaming.tech>
2022-11-30 12:15:04 +0100

    multiple additions

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php 
+require_once "config.php"; 
+$sql = "INSERT INTO contact(fname,lname,email,phone,object,message) VALUES(:fname,:lname,:email,:phone,:object,:message)";
+$dataBinded=array(
+    ':fname'   => $_POST['fname'],
+    ':lname'   => $_POST['lname'],
+    ':email'   => $_POST['email'],
+    ':phone'   => $_POST['phone'],
+    ':object'   => $_POST['object'],
+    ':message'   => $_POST['message'],
+);
+$pre = $pdo->prepare($sql);
+$pre->execute($dataBinded);
+
+header('Location:index.php');
+?>
