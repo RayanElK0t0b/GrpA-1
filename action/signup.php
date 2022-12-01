150406943a9f493028599ccabb9a74d5ecc3effc
Rayan El Kotob <relkotob@gaming.tech>
2022-12-02 00:51:49 +0100

    updated signup

--- action/signup.php
+++ action/signup.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
-require_once "../config.php"; 
-$sql = "INSERT INTO user(username,email,password,fname,lname) VALUES(:username,:email,:password,:fname,:lname)";
+require_once "../config.php";
+$sql = "INSERT INTO user(username,email,password,fname,lname,admin) VALUES(:username,:email,:password,:fname,:lname,0)";
 $dataBinded=array(
     ':username'=> $_POST['username'],
     ':email'   => $_POST['email'],
     ':password'=> $_POST['password'],
     ':fname'   => $_POST['fname'],
-    ':lname'   => $_POST['lname'],
+    ':lname'   => $_POST['lname']
 );
 $pre = $pdo->prepare($sql);
 $pre->execute($dataBinded);
