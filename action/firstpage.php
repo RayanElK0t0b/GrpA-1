28fcdcf4ad9415520ff718d65ad46a22016604a2
Perlainpin_ <greboul@gaming.tech>
2022-12-01 20:29:19 +0100

    mod

--- action/firstpage.php
+++ action/firstpage.php
@@ -1,21 +1,32 @@
-<?php     
+<?php require_once "config.php";
 
-    $sql = "SELECT * FROM home";     
-    $pre = $pdo->prepare($sql);     
-    $pre->execute();     
-    $data = $pre->fetch(PDO::FETCH_ASSOC);        
-    
-?>   
-  
-<form class="" action="php\updatehome_page.php" method="post" enctype="multipart/form-data">       
-    <textarea name="h1"> <?php echo $data['h1'] ?></textarea>       
-    <textarea name="h2"> <?php echo $data['h2'] ?></textarea>       
-    <textarea name="p1"> <?php echo $data['p1'] ?></textarea>       
-    <textarea name="p2"> <?php echo $data['p2'] ?></textarea>        
-    
-    <img src="<?php echo $data['img1'] ?>" width="50px">       
-    <input type="file" name="img1">       
-    <img src="<?php echo $data['img2'] ?>" width="50px">       
-    <input type="file" name="img2">        
-    
-    <button type="submit" name="button">Modifier</button>
+  $sql = "UPDATE home SET title=:h1, navbarsec1=:h2, navbarsec2 =:p1, p2 =:p2 ";
+  $dataBinded=array(
+
+  ':h1'=>$_POST['h1'],
+  ':h2'=>$_POST['h2'],
+  ':p1'=>$_POST['p1'],
+  ':p2'=>$_POST['p2'],
+
+
+  );
+  if (!empty($_FILES['img1']['name'])){
+    $destination = "img/".$_FILES['img1']['name']; //dossier "upload"
+    move_uploaded_file($_FILES['img1']['tmp_name'],"../".$destination);
+    $sql=$sql.", img1=:img1";
+    $dataBinded[':img1']=$destination;
+  }
+
+  if (!empty($_FILES['img2']['name'])){
+    $destination = "img/".$_FILES['img2']['name']; //dossier "upload"
+    move_uploaded_file($_FILES['img2']['tmp_name'],"../".$destination);
+    $sql=$sql.", img2=:img2";
+    $dataBinded[':img2']=$destination;
+  }
+
+  $pre = $pdo->prepare($sql); //on prévient la base de données qu'on va executer une requête
+    $pre->execute($dataBinded);
+
+      header('Location:..\panel_admin.php');
+      exit();
+?>
