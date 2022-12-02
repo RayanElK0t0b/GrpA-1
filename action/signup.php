b0a2267b5dd0d0b59ce8b954a881cd79dc5a42d1
Rayan El Kotob <relkotob@gaming.tech>
2022-12-02 01:10:28 +0100

    aled

--- action/signup.php
+++ action/signup.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "../config.php";
-$sql = "INSERT INTO user(username,email,password,fname,lname,admin) VALUES(:username,:email,:password,:fname,:lname,0)";
+$sql = "INSERT INTO user(username,email,password,first_name,last_name,admin) VALUES(:username,:email,:password,:first_name,:last_name,0)";
 $dataBinded=array(
     ':username'=> $_POST['username'],
     ':email'   => $_POST['email'],
