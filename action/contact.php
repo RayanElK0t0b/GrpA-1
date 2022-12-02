8d503c95ddae2664b4623bf5b1afd191f68d9461
Perlainpin_ <greboul@gaming.tech>
2022-12-02 02:57:43 +0100

    mod

--- action/contact.php
+++ action/contact.php
@@ -1,36 +1,18 @@
 <?php 
 
 require_once "../config.php";
+     
+$email = $_POST["email"];
+$objet = $_POST['object'];
+$content = $_POST['message'];
+$headers = "From:".$_POST["email"]."\r\n Reply-To:".$_POST["email"]."\r\n X-Mailer: PHP/".phpversion();
 
-if(is_int($_POST['phone'])){
-    $sql = "INSERT INTO contact VALUES(:fname,:lname,:email,:phone,:obj,:msg)";
-    $dataBinded=array(
-        ':fname'   => $_POST['fname'],
-        ':lname'   => $_POST['lname'],
-        ':email'   => $_POST['email'],
-        ':phone'   => $_POST['phone'],
-        ':obj'   => $_POST['object'],
-        ':msg'   => $_POST['message'],
-    );
-    $sql = $sql.";";
-    $pre = $pdo->prepare($sql);
-    $pre->execute($dataBinded);
-    
-    $email = $_POST['email'];
-    $objet = $_POST['object'];
-    $content = $_POST['message'];
-    $headers = array('MIME-Version: 1.0','Content-type: text/html; charset=utf8');
-
-    if(mail($email,$objet,$content,$headers)){
-        //le mail est bien parti ! Pas d'erreur
-    }else{
-     //on a une erreur quelque part
-    }
-
+if(mail($email,$objet,$content,$headers)){
+//le mail est bien parti ! Pas d'erreur
 }else{
-    //header('Location:../index.php');
-    exit();
+//on a une erreur quelque part
 }
+
 
 //header('Location:../index.php');
 exit();
